09d2cf1f25df0ca212ea66a161fa2de11e258656
karavaevitalii <fkve97@gmail.com>
2016-12-08 21:31:56 +0300

    removed

---
+++ fall-2016/big-integer/Debug/CMakeFiles/big_integer_testing.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/big_integer_testing.dir/src/big_integer.cpp.o"
+  "CMakeFiles/big_integer_testing.dir/test/big_integer_testing.cpp.o"
+  "CMakeFiles/big_integer_testing.dir/src/storage.cpp.o"
+  "CMakeFiles/big_integer_testing.dir/test/gtest/gtest-all.cc.o"
+  "CMakeFiles/big_integer_testing.dir/test/gtest/gtest_main.cc.o"
+  "big_integer_testing.pdb"
+  "big_integer_testing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/big_integer_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ fall-2016/exam/persistent-set/build/CMakeFiles/persistent-set.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/persistent-set.dir/main.cpp.o"
+  "CMakeFiles/persistent-set.dir/persistent_set.cpp.o"
+  "persistent-set.pdb"
+  "persistent-set"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/persistent-set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
