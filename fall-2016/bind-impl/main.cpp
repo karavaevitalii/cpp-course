55f72daff77858aebb4a4a465e2afc24940f54e2
karavaevitalii <fkve97@gmail.com>
2016-12-13 22:46:41 +0300

    small fixes

--- fall-2016/bind-impl/main.cpp
+++ fall-2016/bind-impl/main.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+struct F
+{
+    int operator()(int a, int b)
+    {
+        return  a - b;
+    }
+};
+
 int f(int a, int b)
 {
     return a + b;
@@ -16,7 +24,11 @@ int g(int a, int b)
 
 int main()
 {
-    auto b = bind(&g, (*f)(2, 3), (*g)(2, 3));
-    cout << b(2, 3) << endl;
+    auto b = bind(F(), (*f)(2, 3), (*g)(2, 3));
+    auto c = bind(&f, (*g)(2, 3), (*f)(2, 3));
+    cout << b() << endl;
+    cout << F()(2, 3) << endl;
+    cout << f((*g)(2, 3), f(2, 3)) << endl;
+    cout << c() << endl;
     return 0;
 }
