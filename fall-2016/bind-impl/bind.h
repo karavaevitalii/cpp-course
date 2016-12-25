98846f4e4c90b212f72dbeeff20f54ca00159af2
karavaevitalii <fkve97@gmail.com>
2016-12-25 22:53:57 +0300

    small changes

--- fall-2016/bind-impl/bind.h
+++ fall-2016/bind-impl/bind.h
@@ -6,85 +6,96 @@
 
 namespace bind
 {
-    template<size_t N>
-    struct placeholder
+namespace placeholder
+{
+
+template<size_t N>
+struct placeholder
+{};
+
+placeholder<0> _1;
+placeholder<1> _2;
+placeholder<2> _3;
+
+}
+}
+
+namespace bind
+{
+
+template<typename F, typename ... Args>
+struct bind_t
+{
+    template<typename ... Args1>
+    constexpr auto operator()(Args1&& ... args1)
+    {
+        constexpr typename geter<sizeof ... (Args)>::next arguments;
+        return call(arguments, std::forward<Args1>(args1) ...);
+    }
+
+private:
+    typedef typename std::decay<F>::type func_t;
+    typedef std::tuple<Args ...>         tuple_t;
+
+    bind_t(F&& func, Args&& ... args)
+        : func(std::forward<F>(func))
+        , args(std::forward<Args>(args) ...)
+    {}
+
+    template<typename Arg, typename ... Args1>
+    constexpr auto&& getarg(Arg& a, Args1& ...)
+    {
+        return a;
+    }
+
+    template<size_t N, typename ... Args1>
+    constexpr auto&& getarg(placeholder::placeholder<N>&, Args1& ... args1)
+    {
+        return std::get<N>(std::forward_as_tuple(args1 ...));
+    }
+
+    template<typename B, typename ... Args1, typename ... Args2>
+    constexpr auto getarg(bind_t<B, Args1 ...>& b, Args2& ... args2)
+    {
+        return b(args2 ...);
+    }
+
+    template<size_t ... N>
+    struct arguments
     {};
 
-    placeholder<0> _1;
-    placeholder<1> _2;
-    placeholder<2> _3;
-
-    template<typename F, typename ... Args>
-    struct bind_t
+    template<size_t N, size_t ... Seq>
+    struct geter
     {
-        template<typename ... Args1>
-        constexpr auto operator()(Args1&& ... args1)
-        {
-            return call(typename geter<std::tuple_size<tuple_t>::value>::next()
-                        , std::forward<Args1>(args1) ...);
-        }
-
-    private:
-        typedef typename std::decay<F>::type func_t;
-        typedef std::tuple<Args ...> tuple_t;
-
-        bind_t(F&& func, Args&& ... args)
-            : func(std::forward<F>(func))
-            , args(std::forward<Args>(args) ...)
-        {}
-
-        template<typename Arg, typename ... Args1>
-        constexpr auto&& getarg(Arg& arg, Args1& ...)
-        {
-            return arg;
-        }
-
-        template<size_t N, typename ... Args1>
-        constexpr auto&& getarg(placeholder<N>&, Args1& ... args1)
-        {
-            return std::get<N>(std::forward_as_tuple(args1 ...));
-        }
-
-        template<typename B, typename ... Args1, typename ... Args2>
-        constexpr auto getarg(bind_t<B, Args1 ...>& b, Args2& ... args2)
-        {
-            return b(args2 ...);
-        }
-
-        template<size_t ... N>
-        struct arguments
-        {};
-
-        template<size_t N, size_t ... Seq>
-        struct geter
-        {
-            typedef typename geter<N - 1, N - 1, Seq ...>::next next;
-        };
-
-        template<size_t ... Seq>
-        struct geter<0, Seq ...>
-        {
-            typedef arguments<Seq ...> next;
-        };
-
-        template<size_t ... N, typename ... Args1>
-        constexpr auto call(arguments<N ...> const&, Args1&& ... args1)
-        {
-            return func(getarg(std::get<N>(args), args1 ...) ...);
-        }
-
-        template<typename Fn, typename ... Args1>
-        friend bind_t<Fn, Args1 ...> bind(Fn&& f, Args1&& ... args);
-
-        func_t func;
-        tuple_t args;
+        typedef typename geter<N - 1, N - 1, Seq ...>::next next;
     };
 
-    template<typename F, typename ... Args>
-    bind_t<F, Args ...> bind(F&& f, Args&& ... args)
+    template<size_t ... Seq>
+    struct geter<0, Seq ...>
     {
-        return bind_t<F, Args ...>(std::forward<F>(f), std::forward<Args>(args) ...);
+        typedef arguments<Seq ...> next;
+    };
+
+    template<size_t ... N, typename ... Args1>
+    constexpr auto call(arguments<N ...> const&, Args1&& ... args1)
+    {
+        return func(getarg(std::get<N>(args), args1 ...) ...);
     }
+
+    template<typename Fn, typename ... Args1>
+    friend bind_t<Fn, Args1 ...> bind(Fn&& f, Args1&& ... args);
+
+private:
+    func_t func;
+    tuple_t args;
+};
+
+template<typename F, typename ... Args>
+bind_t<F, Args ...> bind(F&& f, Args&& ... args)
+{
+    return bind_t<F, Args ...>(std::forward<F>(f), std::forward<Args>(args) ...);
+}
+
 }
 
 #endif
--- fall-2016/bind-impl/main.cpp
+++ fall-2016/bind-impl/main.cpp
@@ -4,7 +4,7 @@
 
 struct F
 {
-    int operator()() const
+    int operator()()
     {
         return  4;
     }
@@ -20,6 +20,11 @@ int g(int a, int b)
     return a * b;
 }
 
+int sum(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
     auto a = [](int a, int b) {
@@ -27,7 +32,7 @@ int main()
     };
     auto b = bind::bind(a, bind::bind(F()), bind::bind(F()));
     auto c = bind::bind(f, 4, 5);
-    auto e = bind::bind(&g, bind::_1, bind::_2);
+    auto e = bind::bind(g, bind::placeholder::_1, bind::placeholder::_2);
     std::cout << b() << '\n';
     std::cout << c() << '\n';
     std::cout << e(3, 4) << '\n';
@@ -41,6 +46,15 @@ int main()
     bb();
     bb();
     std::cout << x << '\n';
+
+    namespace bp = bind::placeholder;
+
+    std::cout << (bind::bind(&sum, 5, -5))() << std::endl;
+    std::cout << (bind::bind(&sum, bp::_1, 5))(-5) << std::endl;
+    std::cout << (bind::bind(&sum, bp::_1, bp::_1))(0) << std::endl;
+    std::cout << (bind::bind(&sum, bp::_2, bp::_2))(10, 0) << std::endl;
+    std::cout << (bind::bind(&sum, bp::_2, bp::_1))(5, -5) << std::endl;
+    std::cout << (bind::bind(&sum, bind::bind(&sum, bp::_2, 10), bp::_1))(5, -15) << std::endl;
 
     return 0;
 }
